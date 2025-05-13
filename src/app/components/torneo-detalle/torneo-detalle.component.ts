9111e15e8c9ddf72b435156f59d444b32841e065
lautaroeberle <louloufutbol@gmail.com>
2025-05-13 18:51:58 -0300

    ya se puede ver los detalles-torneo

--- src/app/components/torneo-detalle/torneo-detalle.component.ts
+++ src/app/components/torneo-detalle/torneo-detalle.component.ts
@@ -1,43 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { TorneoService } from '../../services/torneo/torneo.service'; // Ajusta la ruta si es necesario
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { TorneoService } from '../../services/torneo/torneo.service';
+import { CommonModule,Location } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-torneo-detalle',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './torneo-detalle.component.html',
-   standalone: true,
-    imports: [CommonModule,FormsModule],
   styleUrls: ['./torneo-detalle.component.css']
 })
 export class TorneoDetalleComponent implements OnInit {
-  torneo: any; // Define una variable para almacenar el torneo
+  torneo: any;
+  location: any;
 
   constructor(
-    private torneoService: TorneoService, // Inyectar el servicio
-    private route: ActivatedRoute // Inyectar ActivatedRoute para obtener el ID de la URL
+    private route: ActivatedRoute,
+    private torneoService: TorneoService
   ) {}
 
-  ngOnInit(): void {
-    const torneoId = this.route.snapshot.paramMap.get('id'); // Obtener el ID de la URL
-    if (torneoId) {
-      const idNumero = Number(torneoId); // Convertir el id a número
-      if (!isNaN(idNumero)) {
-        this.torneoService.obtenerPorId(idNumero).subscribe({
-          next: (data) => {
-            this.torneo = data; // Asignar los datos al objeto torneo
-            console.log('Torneo cargado:', this.torneo); // Para debuguear
-          },
-          error: (err) => {
-            console.error('Error al obtener el torneo', err); // Manejo de errores
-          }
-        });
-      } else {
-        console.error('ID de torneo inválido');
-      }
-    } else {
-      console.error('ID de torneo no encontrado en la URL');
-    }
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.torneoService.obtenerUno(id).subscribe((data) => {
+      this.torneo = data;
+    });
+  }
+   volver() {
+    this.location.back();
   }
 }
